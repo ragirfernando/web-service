74cef6213aba7fdd438031bc7e0a1f08987fa58b
Ragir Fernando Aparecido da Silva <ragirfernando@gmail.com>
2020-09-17 17:02:52 -0300

    User insert

--- src/main/java/com/webservice/webservice/controller/UserController.java
+++ src/main/java/com/webservice/webservice/controller/UserController.java
@@ -4,11 +4,10 @@ import com.webservice.webservice.domain.User;
 import com.webservice.webservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,16 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable Integer id)  {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
